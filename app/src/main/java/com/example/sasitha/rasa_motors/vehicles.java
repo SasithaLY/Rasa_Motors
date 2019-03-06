0a47bbc291214b44074b2340c0f203c5d9cf9de4
SasithaLY <onlinesasitha@gmail.com>
2019-03-06 12:36:52 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sasitha/rasa_motors/vehicles.java
@@ -0,0 +1,13 @@
+package com.example.sasitha.rasa_motors;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class vehicles extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vehicles);
+    }
+}
